09b9e7115495cde26bd65672ac167e2806268568
JeongYunHo <uno8941@gmail.com>
2021-10-31 19:37:47 +0900

    add styled-reset, globalstyles

--- src/routes/select_category.tsx
+++ src/routes/select_category.tsx
@@ -3,5 +3,17 @@ import React from "react";
 interface Props {}
 
 export const SelectCategory: React.FC<Props> = () => {
-  return <div>select category</div>;
+  return (
+    <ul>
+      <li>
+        <button>자유</button>
+      </li>
+      <li>
+        <button>관계</button>
+      </li>
+      <li>
+        <button>직업</button>
+      </li>
+    </ul>
+  );
 };
