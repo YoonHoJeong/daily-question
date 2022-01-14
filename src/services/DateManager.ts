3cb5fb18f7192fdff38f8e8c2f918babdceb6507
JeongYunHo <uno8941@gmail.com>
2022-01-14 18:29:09 +0900

    add date converter

---
+++ src/services/DateManager.ts
@@ -0,0 +1,17 @@
+function pad(num: Number): string {
+  return num < 10 ? `0${num}` : `${num}`;
+}
+
+export const convertDate = (date: Date) => {
+  // convert Date instance into format yyyy-mm-ddThh:mm:ss
+  const y = pad(date.getFullYear());
+  const m = pad(date.getMonth() + 1);
+  const d = pad(date.getDate());
+  const h = pad(date.getHours());
+  const min = pad(date.getMinutes());
+  const sec = pad(date.getSeconds());
+
+  return `${y}-${m}-${d}T${h}:${min}:${sec}`;
+};
+
+// week's format - yyyy-mmWw
