57ba094beeba0dc560d61a7359687f44518cb603
JeongYunHo <uno8941@gmail.com>
2022-01-18 20:57:52 +0900

    add logout, formatuser, admin flag

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,21 +2,24 @@ import {
   getAuth,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  User,
 } from "firebase/auth";
 import React, { useContext, useEffect, useState } from "react";
 import { firebaseApp } from "../services/firebase";
 
 interface Props {}
 interface Auth {
-  user: User | null;
+  user: CustomUser | null;
   isAuthenticating: boolean;
   login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
 }
 
-interface User {
+interface CustomUser {
   name: string;
   email?: string | null;
   uid: string;
+  admin: boolean;
 }
 
 export const AuthContext = React.createContext<Auth | null>(null);
@@ -27,18 +30,32 @@ export const AuthProvider: React.FC<Props> = ({ children }) => {
 };
 
 const useProviderAuth = () => {
-  const [user, setUser] = useState<User | null>(null);
+  const [user, setUser] = useState<CustomUser | null>(null);
   const [isAuthenticating, setIsAuthenticating] = useState(true);
   const auth = getAuth(firebaseApp);
 
-  const login = async (email: string, password: string) => {
-    try {
-      const { user } = await signInWithEmailAndPassword(auth, email, password);
+  const formatUser = async (user: User | null) => {
+    if (user) {
+      const token = await user.getIdTokenResult();
+
       setUser({
         name: user.displayName || "undefined",
         email: user.email,
         uid: user.uid,
+        admin: token.claims.admin ? true : false,
       });
+    } else {
+      setUser(null);
+    }
+
+    setIsAuthenticating(false);
+  };
+
+  const login = async (email: string, password: string) => {
+    try {
+      const { user } = await signInWithEmailAndPassword(auth, email, password);
+
+      formatUser(user);
     } catch (e: any) {
       let error = null;
       if (e.code) {
@@ -55,29 +72,31 @@ const useProviderAuth = () => {
     }
   };
 
+  const logout = async () => {
+    try {
+      await auth.signOut();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, (user) => {
       if (user) {
         // When user is signed in.
         console.log("user logged in", auth);
-        setUser({
-          name: user.displayName || "undefined",
-          email: user.email,
-          uid: user.uid,
-        });
-        setIsAuthenticating(false);
+        formatUser(user);
       } else {
         // When User is not signed in.
         console.log("logged out", auth);
-        setUser(null);
-        setIsAuthenticating(false);
+        formatUser(null);
       }
     });
 
     return () => unsub();
   }, [auth]);
 
-  return { user, isAuthenticating, login };
+  return { user, isAuthenticating, login, logout };
 };
 
 export const useAuth = () => {
