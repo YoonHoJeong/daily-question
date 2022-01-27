2ae19b63647a8fcfbdd9ee2915fb6a240add7da4
JeongYunHo <uno8941@gmail.com>
2022-01-27 14:58:00 +0900

    add hooks/usePreloadImages for preload images before page load

---
+++ src/hooks/usePreloadImages.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+export const usePreloadImages = (images: string[]) => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const preloadImages = async (images: string[]) => {
+      const promises = await images.map(
+        (image) =>
+          new Promise<void>(function (resolve, reject) {
+            const imageObj = new Image();
+            imageObj.src = image;
+            imageObj.onload = () => resolve();
+            imageObj.onerror = () => reject();
+          })
+      );
+      await Promise.all(promises);
+      setLoading(false);
+    };
+    preloadImages(images);
+  }, []);
+
+  return { loading };
+};
