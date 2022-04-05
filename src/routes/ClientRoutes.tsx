67d6c0e9a6d064f92233f3b1f7c7a650da281aeb
JeongYunHo <uno8941@gmail.com>
2022-04-05 14:12:23 +0900

    add LazyLoading

--- src/routes/ClientRoutes.tsx
+++ src/routes/ClientRoutes.tsx
@@ -1,22 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 import { Switch, Route } from "react-router-dom";
 import { Header, BottomNavigation } from "../layouts";
-import {
-  Answers,
-  Question,
-  SubmitDone,
-  Home,
-  User,
-  Board,
-  UserEdit,
-} from "../pages";
+// import {
+//   Answers,
+//   Question,
+//   SubmitDone,
+//   Home,
+//   User,
+//   Board,
+//   UserEdit,
+// } from "../pages";
 import { ErrorBoundary } from "../components/common/ErrorBoundary";
 import sizes from "../layouts/sizes";
+const Answers = React.lazy(() => import("../pages/Answers/Answers"));
+const Question = React.lazy(() => import("../pages/Question/Question"));
+const SubmitDone = React.lazy(() => import("../pages/SubmitDone/SubmitDone"));
+const Home = React.lazy(() => import("../pages/Home/Home"));
+const User = React.lazy(() => import("../pages/User/User"));
+const Board = React.lazy(() => import("../pages/Board/Board"));
+const UserEdit = React.lazy(() => import("../pages/User/UserEdit"));
 
 interface Props {}
 
 const ClientRoutes: React.FC<Props> = () => {
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      // We execute the same script as before
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  }, []);
+
   return (
     <Container>
       <Header />
@@ -66,7 +81,6 @@ const ClientRoutes: React.FC<Props> = () => {
 };
 
 const viewHeight = window.innerHeight;
-const mainHeight = viewHeight - 108;
 
 const Container = styled.div`
   width: 100vw;
@@ -75,7 +89,9 @@ const Container = styled.div`
 `;
 
 const Main = styled.main`
-  height: ${mainHeight}px;
+  padding-top: 48px;
+  padding-bottom: 60px;
+  height: calc(var(--vh, 1vh) * 100);
   background-color: ${({ theme }) => theme.palette.bgGrey};
 `;
 
