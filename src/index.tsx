ea0b0c4b21826e7d03e5e0f2454c339fcabd7a35
JeongYunHo <uno8941@gmail.com>
2022-04-05 14:17:31 +0900

    add sentry to tracking errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
 import "./index.css";
+
+Sentry.init({
+  dsn: "https://84405297d7df47b18b8c73622700e2a0@o1189345.ingest.sentry.io/6309689",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
