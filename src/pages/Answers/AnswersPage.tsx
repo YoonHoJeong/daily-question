0fc6f5c10e7bc1e349b51adfc3682fdd1f1208ad
JeongYunHo <uno8941@gmail.com>
2022-04-19 14:27:05 +0900

    add: navigate to weekly when no matched route

--- src/pages/Answers/AnswersPage.tsx
+++ src/pages/Answers/AnswersPage.tsx
@@ -1,5 +1,5 @@
-import React, { createContext, Suspense } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { Suspense } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import DailyAnswersPage from './DailyAnswersPage';
 import MonthlyAnswersPage from './MonthlyAnswersPage';
@@ -8,9 +8,9 @@ import { Link } from 'react-router-dom';
 
 import { ClientLayout } from '../../components/layouts/ClientLayout';
 import { BottomNavigation, Header } from '../../components/layouts';
-import { DateFormatProvider } from '../../hooks/useDateFormat';
 import { LoadScreen } from '../../components/common';
 import { ErrorBoundary } from '../../components/common/ErrorBoundary';
+import DateFormatPicker from '../../components/DateFormatPicker';
 
 interface Props {}
 
@@ -18,47 +18,47 @@ const AnswersPage: React.FC<Props> = () => {
   return (
     <ClientLayout style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <Header />
-      <DateFormatProvider>
-        <Link to="daily">일간</Link>
-        <Link to="weekly">주간</Link>
-        <Link to="monthly">월간</Link>
-        <ErrorBoundary>
-          <Routes>
-            <Route
-              path=""
-              element={
-                <Suspense fallback={LoadScreen}>
-                  <WeeklyAnswersPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="weekly"
-              element={
-                <Suspense fallback={LoadScreen}>
-                  <WeeklyAnswersPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="daily"
-              element={
-                <Suspense fallback={LoadScreen}>
-                  <DailyAnswersPage />
-                </Suspense>
-              }
-            />
-            <Route
-              path="monthly"
-              element={
-                <Suspense fallback={LoadScreen}>
-                  <MonthlyAnswersPage />
-                </Suspense>
-              }
-            />
-          </Routes>
-        </ErrorBoundary>
-      </DateFormatProvider>
+      <Link to="daily">일간</Link>
+      <Link to="weekly">주간</Link>
+      <Link to="monthly">월간</Link>
+      <DateFormatPicker />
+      <ErrorBoundary>
+        <Routes>
+          <Route
+            path=""
+            element={
+              <Suspense fallback={LoadScreen}>
+                <WeeklyAnswersPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="weekly"
+            element={
+              <Suspense fallback={LoadScreen}>
+                <WeeklyAnswersPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="daily"
+            element={
+              <Suspense fallback={LoadScreen}>
+                <DailyAnswersPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="monthly"
+            element={
+              <Suspense fallback={LoadScreen}>
+                <MonthlyAnswersPage />
+              </Suspense>
+            }
+          />
+          <Route path="*" element={<Navigate to="weekly" replace />} />
+        </Routes>
+      </ErrorBoundary>
       <BottomNavigation />
     </ClientLayout>
   );
